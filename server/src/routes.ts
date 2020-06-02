2fc93782a2fdcf08f55272eb32c676ae9b3ea3ac
gabrielrportugal <gabrielrportugal@outlook.com>
2020-06-02 16:38:56 -0300

    Second day finished

---
+++ server/src/routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router();
+const poinstController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', poinstController.create);
+routes.get('/points', poinstController.index);
+routes.get('/points/:id', poinstController.show);
+
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,15 @@
 import express  from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usários')
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  response.json([
-    'Gabriel',
-    'Arthur',
-    'Diego',
-  ]);
-} );
+app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')));
 
 app.listen(3333);
